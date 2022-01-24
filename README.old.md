1aad77256b00a3ac8d083372411c8e2ae0df2b7a
keshav <keshav1555@gmail.com>
2022-01-24 18:20:21 +1100

    Completing the practice website

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# PortfolioWebsitePractice
+Learning how to create my own portfolio website using https://www.youtube.com/watch?v=7WwtzsSHdpI
---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Intro from "./components/intro/intro";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Topbar from "./components/topbar/Topbar";
+import Works from "./components/works/works";
+import Testimonials from "./components/testimonials/testimonials";
+import Contact from "./components/contact/contact"
+import "./app.scss"
+import { useState } from "react";
+import  Menu  from "./components/menu/menu";
+
+function App() {
+  const [menuOpen,setMenuOpen] = useState(false)
+  return (
+    <div className="app">
+      <Topbar menuOpen = {menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen = {menuOpen} setMenuOpen={setMenuOpen}/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Testimonials/>
+        <Contact/>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
